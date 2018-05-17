1baeb0d6869b89798d03723b89069d03f4f47dab
LilankaUdawatta <39363703+LilankaUdawatta@users.noreply.github.com>
2018-05-17 11:50:21 +0530

    Add files via upload

---
+++ AssetBundling.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetBundling : MonoBehaviour {
+
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles()
+	{
+		//string assetBundleDirectory = "Assets/StreamingAssets/AssetBundles";
+		BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+	}
+}
+#endif
